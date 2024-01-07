f3d7c791902623422ac9baec5726ef2fdbe906c4
Raymond Tao <taor10@mcmaster.ca>
2024-01-06 19:51:17 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import VisDev from './pages/VisDev';
-import Personal from './pages/Personal';
-import AboutContact from './pages/AboutContact';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import './App.css';
-import './styles/global.css';
+import React from "react";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
+import VisDev from "./pages/VisDev";
+import Personal from "./pages/Personal";
+import AboutContact from "./pages/AboutContact";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import "./App.css";
+import "./styles/global.css";
 
 function App() {
   return (
-    <Router>
+    <>
       <NavBar />
       <Routes>
         <Route path="/" element={<VisDev />} />
@@ -18,7 +18,7 @@ function App() {
         <Route path="/about-contact" element={<AboutContact />} />
       </Routes>
       <Footer />
-    </Router>
+    </>
   );
 }
 
